4734b2504772cf5da62528cf53daf5782bf5879c
ibmuser <ibmuser@IBM657-R9022R5M.in.ibm.com>
2018-08-01 13:48:42 +0530

    Palindrome initial check in

---
+++ Java8Programs/src/Palindrome/Smallest.java
@@ -0,0 +1,12 @@
+package Palindrome;
+
+public class Smallest {
+
+	public static void main(String[] args) {
+
+		String input = "23245345123234434";
+
+		System.out.println("Smallest palindrome in " + input + " is : " + input);
+	}
+
+}
