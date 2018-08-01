3432f31f56123a5cbe5c103078b6fba542fb8287
ibmuser <ibmuser@IBM657-R9022R5M.in.ibm.com>
2018-08-01 14:03:24 +0530

    Logic Commit

--- Java8Programs/src/Palindrome/Smallest.java
+++ Java8Programs/src/Palindrome/Smallest.java
@@ -1,12 +1,20 @@
 package Palindrome;
 
+import java.util.TreeSet;
+
 public class Smallest {
 
 	public static void main(String[] args) {
 
-		String input = "23245345123234434";
+		String input = "233255345123234434";
 
-		System.out.println("Smallest palindrome in " + input + " is : " + input);
+		TreeSet<Integer> tree = new TreeSet<Integer>();
+		for (int i = 0; i < input.length() - 1; i++) {
+			if (input.charAt(i) == input.charAt(i + 1)) {
+				tree.add(Integer.parseInt(input.substring(i, i + 2)));
+			}
+		}
+		System.out.println("Smallest palindrome in " + input + " is : " + tree.first());
+		System.out.println("Treeset values : " + tree);
 	}
-
 }
